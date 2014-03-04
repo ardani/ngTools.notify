66a1585cb42617f5ad7053231bb6fa9ff16483cd
mrobin <mrobin@companeo.com>
2014-03-04 15:18:00 +0100

    better service + tests

--- src/notify.js
+++ src/notify.js
@@ -18,24 +18,48 @@ angular.module('Notify', [])
         function ($timeout) {
             'use strict';
 
-            var service = {
-                list : {},
-                add  : function (message) {
-                    var timestamp = (new Date()).getTime(),
-                        closable = message.closable || (undefined === message.delay);
+            var exception = '[ngTools.Notify] Undefined or misformated message.',
+                service = {
+                    list : {},
+                    add  : function (message) {
+                        var timestamp,
+                            closable,
+                            message_tmp;
 
-                    service.list[timestamp] = {
-                        text      : message.text,
-                        status    : message.status,
-                        closable  : closable,
-                        timestamp : timestamp || null
-                    };
+                        if ('string' === typeof message) {
+                            message_tmp = {
+                                text : message
+                            };
+                        } else {
+                            message_tmp = message;
+                        }
 
-                    $timeout(function () {
-                        delete service.list[timestamp];
-                    }, (message.delay || 5000));
-                }
-            };
+                        try {
+                            if ('string' === typeof message_tmp.text) {
+                                timestamp = (new Date()).getTime();
+                                closable = message_tmp.closable || (undefined === message_tmp.delay);
+                                status = message_tmp.status || 'success';
+
+                                service.list[timestamp] = {
+                                    text      : message_tmp.text,
+                                    status    : status,
+                                    closable  : closable,
+                                    timestamp : timestamp
+                                };
+
+                                $timeout(function () {
+                                    delete service.list[timestamp];
+                                }, (message_tmp.delay || 5000));
+                            } else {
+                                throw exception;
+                            }
+                        } catch ($e) {
+                            throw exception;
+                        }
+
+                        return service.list[timestamp];
+                    }
+                };
             return service;
         }
     ]);
--- tests/service.js
+++ tests/service.js
@@ -11,15 +11,110 @@ describe('Service : Notify', function () {
         service = notify;
     }));
 
-    describe('notify', function () {
-        it('should be correctly defined', function () {
-            expect(service).toBeDefined();
-            expect(typeof service).toBe('object');
-            expect(Object.keys(service).length).toBe(2);
-            expect(service.add).toBeDefined();
-            expect(typeof service.add).toBe('function');
-            expect(service.list).toBeDefined();
-            expect(typeof service.list).toBe('object');
-        });
+    it('should be correctly defined', function () {
+        expect(service).toBeDefined();
+        expect(typeof service).toBe('object');
+        expect(Object.keys(service).length).toBe(2);
+        expect(service.add).toBeDefined();
+        expect(typeof service.add).toBe('function');
+        expect(service.list).toBeDefined();
+        expect(typeof service.list).toBe('object');
+    });
+
+    it('should throw an exception cause of a non-object message', function () {
+        expect(function () {
+            service.add();
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(null);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(0);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(1);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(Infinity);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(-Infinity);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(NaN);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add({});
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add([]);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(true);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+        expect(function () {
+            service.add(false);
+        }).toThrow('[ngTools.Notify] Undefined or misformated message.');
+    });
+
+    it('should not throw', function () {
+        expect(function () {
+            service.add({
+                text : 'a'
+            });
+        }).not.toThrow();
+        expect(function () {
+            service.add('a');
+        }).not.toThrow();
+        expect(function () {
+            service.add('0');
+        }).not.toThrow();
+    });
+
+    it('should add a simple message', function () {
+        var timestamp_tmp = (new Date()).getTime(),
+            message_tmp = service.add({
+                text : 'a'
+            });
+
+        expect(message_tmp).toBeDefined();
+        expect(typeof message_tmp).toBe('object');
+        expect(Object.keys(message_tmp).length).toBe(4);
+        expect(message_tmp.text).toBeDefined();
+        expect(typeof message_tmp.text).toBe('string');
+        expect(message_tmp.text).toBe('a');
+        expect(message_tmp.status).toBeDefined();
+        expect(typeof message_tmp.status).toBe('string');
+        expect(message_tmp.status).toBe('success');
+        expect(message_tmp.closable).toBeDefined();
+        expect(typeof message_tmp.closable).toBe('boolean');
+        expect(message_tmp.closable).toBe(true);
+        expect(message_tmp.timestamp).toBeDefined();
+        expect(typeof message_tmp.timestamp).toBe('number');
+        expect(message_tmp.timestamp).toBe(timestamp_tmp);
+    });
+
+    it('should add a message with a status', function () {
+        var timestamp_tmp = (new Date()).getTime(),
+            message_tmp = service.add({
+                text : 'a',
+                status : 'error'
+            });
+
+        expect(message_tmp).toBeDefined();
+        expect(typeof message_tmp).toBe('object');
+        expect(Object.keys(message_tmp).length).toBe(4);
+        expect(message_tmp.text).toBeDefined();
+        expect(typeof message_tmp.text).toBe('string');
+        expect(message_tmp.text).toBe('a');
+        expect(message_tmp.status).toBeDefined();
+        expect(typeof message_tmp.status).toBe('string');
+        expect(message_tmp.status).toBe('error');
+        expect(message_tmp.closable).toBeDefined();
+        expect(typeof message_tmp.closable).toBe('boolean');
+        expect(message_tmp.closable).toBe(true);
+        expect(message_tmp.timestamp).toBeDefined();
+        expect(typeof message_tmp.timestamp).toBe('number');
+        expect(message_tmp.timestamp).toBe(timestamp_tmp);
     });
 });
